1e215a389a3565729e8f3f9ab6a49723c5c6e3f3
Giftmasil <masilagift@gmail.com>
2024-04-26 15:30:11 +0300

    addition and animation of the hambuger menu and transfer of animation to the tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,29 @@ module.exports = {
         '80%': { transform: 'scaleY(1.2)'},
         '100%': { transform: 'scaleY(1)'},
         },
+        'image': {
+          '0%': { transform: 'translateX(0) rotate(0)'},
+          '33%': { transform: 'translateX(-100px) rotate(360deg)'},
+          '66%': { transform: 'translateX(100px) rotate(-360deg)'},
+        '100%': { transform: 'translateX(0) rotate(0)'},
+        },
+        'left-text': {
+          '0%': { transform: 'translateX(-1000px)'},
+          '80%': { transform: 'translateX(1.2)'},
+          '100%': { transform: 'translateX(0)'},
+        },
+        'right-text': {
+          '0%': { transform: 'translateX(1000px)'},
+          '80%': { transform: 'translateX(-1.2)'},
+          '100%': { transform: 'translateX(0)'},
+        },
       },
       animation: {
         'open-menu': 'open-menu 0.5s ease-in-out forwards',
-      }
+        'img-animation': 'image 5s ease 0.5s forwards',
+        'left-text': 'left-text 4.5s ease 0.5s forwards',
+        'right-text': 'right-text 4.5s ease 0.5s forwards',
+      },
     },
   },
   plugins: [],
